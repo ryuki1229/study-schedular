bf3d73eaf7d0b9afa25623c224fdc8032aa84381
ryuki1229 <yrj031629@gmail.com>
2026-08-04 13:47:54 +0900

    1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="route-v1-13-persistence-fix";
+const CACHE="route-v1-13-1-initial-render-fix";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
