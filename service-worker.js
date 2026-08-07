7b43c8eab35f0bd79e2690c5f3a4f608416a2f22
ryuki1229 <yrj031629@gmail.com>
2026-08-07 19:16:39 +0900

    1

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,5 @@
-const CACHE="route-v1-13-1-initial-render-fix";
+const CACHE="route-v1-14-actual-progress";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener("fetch",event=>{
-  if(event.request.method!=="GET")return;
-  event.respondWith(
-    fetch(event.request,{cache:"no-store"})
-      .then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-        return response;
-      })
-      .catch(()=>caches.match(event.request))
-  );
-});;
+self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)))});
